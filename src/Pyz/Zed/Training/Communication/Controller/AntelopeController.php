76a52bf42c36fb2d3c1de27b59efe1fb8f73120b
Hidran Arias <hidran.arias@spryker.com>
2024-08-07 19:52:43 +0200

    Adds AntelopeController and add.twig view to show a just created antelope

---
+++ src/Pyz/Zed/Training/Communication/Controller/AntelopeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pyz\Zed\Training\Communication\Controller;
+
+use Generated\Shared\Transfer\AntelopeTransfer;
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \Pyz\Zed\Training\Business\TrainingFacadeInterface getFacade()
+ */
+class AntelopeController extends AbstractController
+{
+    public function addAction(Request $request): array
+    {
+        $antelopeTransfer = new AntelopeTransfer();
+        $antelopeTransfer->setName($request->get('name') ?? 'Oskar');
+        $this->getFacade()->createAntelope($antelopeTransfer);
+        return $this->viewResponse(['antelope' => $antelopeTransfer]);
+    }
+}
