c7c19a2117e921000e0b288bf405b7c5621bb3bf
Hidran Arias <hidran.arias@spryker.com>
2024-08-07 19:54:47 +0200

    Adds the GateWay controller and exposes the method getAntelopeAction

---
+++ src/Pyz/Zed/Training/Communication/Controller/GatewayController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pyz\Zed\Training\Communication\Controller;
+
+use Generated\Shared\Transfer\AntelopeCriteriaTransfer;
+use Generated\Shared\Transfer\AntelopeResponseTransfer;
+use Spryker\Zed\Kernel\Communication\Controller\AbstractGatewayController;
+
+/**
+ * @method \Pyz\Zed\Training\Business\TrainingFacadeInterface getFacade()
+ */
+class GatewayController extends AbstractGatewayController
+{
+    public function getAntelopeAction(AntelopeCriteriaTransfer $antelopeCriteria
+    ): AntelopeResponseTransfer {
+        return $this->getFacade()
+            ->getAntelope($antelopeCriteria);
+    }
+}
