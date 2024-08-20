36a3f43b9354cb1b349e90ac2d023e0c6bab6239
Hidran Arias <hidran.arias@spryker.com>
2024-08-20 15:55:02 +0200

    Adds Demo01 OMS

---
+++ src/Pyz/Zed/Oms/Communication/Plugin/Command/Demo/PayCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pyz\Zed\Oms\Communication\Plugin\Command\Demo;
+
+use Orm\Zed\Sales\Persistence\SpySalesOrder;
+use Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\AbstractCommand;
+use Spryker\Zed\Oms\Dependency\Plugin\Command\CommandByOrderInterface;
+
+class PayCommand extends AbstractCommand implements CommandByOrderInterface
+{
+    /**
+     *
+     * Command which is executed per order basis
+     *
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem[] $orderItems
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrder $orderEntity
+     * @param \Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject $data
+     *
+     * @return array
+     * @api
+     *
+     */
+    public function run(
+        array $orderItems,
+        SpySalesOrder $orderEntity,
+        ReadOnlyArrayObject $data
+    ): array {
+        return [];
+    }
+}
